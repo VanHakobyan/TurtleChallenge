99190d7e413d8de0c645cc352b14411336a4e0d4
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2018-05-19 19:21:45 +0400

    add printer

--- TurtleChallenge/Turtle.Console/Program.cs
+++ TurtleChallenge/Turtle.Console/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Turtle.Console.Models;
+﻿using Turtle.Console.Models;
 using Turtle.Library;
 
 namespace Turtle.Console
---
+++ TurtleChallenge/Turtle.Library/Printer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+
+namespace Turtle.Library
+{
+    public static class Printer
+    {
+        private const string FacingSouth = "Turtle now facing South";
+        private static string _movedFromTo = "Turtle moved from position {from} to {to}";
+        private const string Danger = "Danger";
+        private const string MineHit = "Mine Hit";
+        private const string Exit = "Exit";
+
+        public static void Print(Point pointFrom, Point pointTo, string text, string action = null)
+        {
+            var combinePointFrom = $"({pointFrom.X},{pointFrom.Y})";
+            var combinePointTo = $"({pointTo.X},{pointTo.Y})";
+            var printText = text.Replace("{from}", combinePointFrom).Replace("{to}", combinePointTo);
+            if (action != null) printText = $"{printText} - {action}";
+            Console.WriteLine(printText);
+        }
+
+        public static void PrintSimple(string text) => Console.WriteLine(text);
+    }
+}
