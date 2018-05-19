533a6074bb906d7cfc85d67591203c1a2bc2ee9c
Aram Zhamkochyan <aramzham@hotmail.com>
2018-05-19 17:03:16 +0400

    asum a pull ara 1

---
+++ TurtleChallenge/Turtle.Console/Models/State.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turtle.Console.Models
+{
+    public class State
+    {
+        public Point Position { get; set; }
+        public string Direction { get; set; }
+    }
+}
