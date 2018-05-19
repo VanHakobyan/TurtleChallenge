4b2831ed78f494030258f756f5be8348ea8b6869
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2018-05-19 17:00:56 +0400

    add folder

--- TurtleChallenge/Turtle.Library/GameActions/Exit.cs
+++ TurtleChallenge/Turtle.Library/GameActions/Exit.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Turtle.Library
+namespace Turtle.Library.GameActions
 {
-    class Turtle
+    public class Exit
     {
     }
 }
