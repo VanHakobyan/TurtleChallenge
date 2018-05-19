e9877192b9f87549dc1c81c3512d9d7d763bed16
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2018-05-19 17:25:12 +0400

    add csvs

---
+++ TurtleChallenge/Turtle.Library/FileReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Turtle.Library
+{
+    public class FileReader
+    {
+        public void GetSimpleSettings()
+        {
+            var moveExit = File.ReadAllText("\\Settings\\move-exit.csv");
+            var moveHitMine = File.ReadAllText("\\Settings\\move-hit mine.csv");
+            var moveNoEnd = File.ReadAllText("\\Settings\\move-no end.csv");
+        }
+
+        public void GetAdvanceSettings()
+        {
+            var settings = File.ReadAllText("\\Settings\\settings.csv");
+        }
+    }
+}
