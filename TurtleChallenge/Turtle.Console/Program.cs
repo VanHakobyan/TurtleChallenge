6799ca1c31a52b654f03419b9c48c7ce49939773
Aram Zhamkochyan <aramzham@hotmail.com>
2018-05-19 19:37:02 +0400

    observer added

--- TurtleChallenge/Turtle.Console/Program.cs
+++ TurtleChallenge/Turtle.Console/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Turtle.Console.Models;
 using Turtle.Library;
@@ -16,6 +17,8 @@ namespace Turtle.Console
             var simpleSettingsModel = fileReader.GetAdvanceSettings();
             var game = Game.CreateNewGame();
             game.Start();
+
+            Thread.Sleep(60000);
         }
     }
 }
